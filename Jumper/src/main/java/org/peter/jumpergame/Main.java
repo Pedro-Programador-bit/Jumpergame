b1f35ed8723d61e90329a04b37131fb7616c1eee
Pedro-Programador-bit <pedroknabach2016@gmail.com>
2026-05-23 13:54:57 -0300

    Criação da classe Peca e da enum Movement

--- Jumper/src/main/java/org/peter/jumpergame/Main.java
+++ Jumper/src/main/java/org/peter/jumpergame/Main.java
@@ -1,5 +1,9 @@
 package org.peter.jumpergame;
 
+/**
+ * @author Pedro Knabach Andrade
+ */
+
 public class Main {
     static void main() {
         IO.println("Jogo não feito ainda!");
---
+++ Jumper/src/main/java/org/peter/jumpergame/Movement.java
@@ -0,0 +1,11 @@
+package org.peter.jumpergame;
+
+/**
+ * @author Pedro Knabach Andrade
+ * Esta enum determina movimentos possíveis pelas peças. Usada na classe {@link Peca}.
+ * O "M" indica movimento e o "A" indica ataque.
+ */
+
+public enum Movement {
+    ACAP, MFR, MBCK, MLFT, MRGT, AJMP
+}
