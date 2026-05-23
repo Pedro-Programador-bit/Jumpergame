99ecdb45017fcb87e1fed6a7ed0e1f458df19ec2
Pedro-Programador-bit <pedroknabach2016@gmail.com>
2026-05-23 15:42:31 -0300

    Criação dos atributos e construtores da classe Peca

--- Jumper/src/main/java/org/peter/jumpergame/Peca.java
+++ Jumper/src/main/java/org/peter/jumpergame/Peca.java
@@ -1,4 +1,41 @@
 package org.peter.jumpergame;
 
+import java.util.ArrayList;
+
+/**
+ * Esta classe serve como pai das peças, tendo os principais atributos.
+ * Ela usa a enum Movement para declarar movimentos possíveis.
+ * @see Movement
+ */
+
 public class Peca {
+    private boolean isEnemy;
+    /**
+     * Lista dos movimentos disponíveis (da enum {@link Movement}).
+     */
+    public ArrayList<Movement> movesAva = new ArrayList<Movement>();
+    /**
+     * Representação da peça no tabuleiro.
+     */
+    private char symbol;
+    /**
+     * Posição Y no tabuleiro.
+     */
+    private byte y;
+    /**
+     * Posição X no tabuleiro.
+     */
+    private byte x;
+    /**
+     * Construtor.
+     */
+    Peca(boolean pIsEnemy, byte pX, byte pY, char pSymbol, Movement[] pMovesAva) {
+        isEnemy = pIsEnemy;
+        x = pX;
+        y = pY;
+        symbol = pSymbol;
+        for (byte ind=0; ind<pMovesAva.length; ind++) {
+            movesAva.add(pMovesAva[ind]);
+        }
+    }
 }
