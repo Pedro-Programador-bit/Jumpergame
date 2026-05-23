7634b3c5ebad591b05f79cd342caaada870d7887
Pedro-Programador-bit <pedroknabach2016@gmail.com>
2026-05-23 12:23:03 -0300

    Adição da classe Peca e do enum Movement

---
+++ src/main/java/org/peter/jumpergame/Movement.java
@@ -0,0 +1,11 @@
+package org.peter.jumpergame;
+
+
+/**
+ * Enum que indica os movimentos possíveis. Muito usado na classe {@link Peca} e em métodos relacionados.
+ * O "A" no ínicio indica ataque, e o "M" indica movimento.
+ */
+
+public enum Movement {
+
+}
